34e8b229b757d4f6d7217bcb6c5f91113018d2a7
Katrin <egosumcorvus@gmail.com>
2021-06-10 13:30:59 +0300

    init

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+import test.Helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Helloworld helloworld = new Helloworld();
+        helloworld.helloWorld();
+
+
+    }
+}
